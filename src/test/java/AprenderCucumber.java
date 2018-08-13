99329e4b0b0e9c3ec665045214817704ae73eed0
Renato Soares <renatof.m.soares@gmail.com>
2018-08-12 22:13:08 -0300

    first commit

---
+++ src/test/java/AprenderCucumber.java
@@ -0,0 +1,40 @@
+import org.junit.Assert;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+
+
+public class AprenderCucumber {
+
+//	public void que_criei_o_arquivo_corretamente() throws Throwable {
+	@Dado("^que criei o arquivo corretamente$")
+	public void queCrieiOArquivoCorretamente() throws Throwable {
+	}
+
+	@Quando("^executá-lo$")
+	public void executáLo() throws Throwable {
+	}
+
+	@Então("^a especificação deve finalizar com sucesso$")
+	public void aEspecificaçãoDeveFinalizarComSucesso() throws Throwable {
+	}
+	
+	private int contador = 0;
+	
+	@Dado("^que o valor do contador é (\\d+)$")
+	public void queOValorDoContadorÉ(int arg1) throws Throwable {
+	    contador = arg1;
+	}
+
+	@Quando("^eu incrementar em (\\d+)$")
+	public void euIncrementarEm(int arg1) throws Throwable {
+	   contador = contador + arg1;
+	}
+
+	@Então("^o valor do contador será (\\d+)$")
+	public void oValorDoContadorSerá(int arg1) throws Throwable {
+	    Assert.assertEquals(arg1, contador);
+	}
+}
